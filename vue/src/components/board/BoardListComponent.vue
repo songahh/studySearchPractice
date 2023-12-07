2245eb7b5814e53d3990e54d8de8679744688773
songahh <gunsong2@gmail.com>
2023-12-07 13:00:20 +0900

    [vue] pagination 추가

--- vue/src/components/board/BoardListComponent.vue
+++ vue/src/components/board/BoardListComponent.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BoardListItemComponent from '@/components/board/item/BoardListItemComponent.vue'
 import { ref } from 'vue'
+import PaginationComponent from '@/components/board/item/PaginationComponent.vue'
 
 /**************************** tab 정보 *****************************/
 const tabsContainer = ref(null)
@@ -39,6 +40,23 @@ function clickTab(clicked, unclickedContent) {
 }
 /******************************************************************/
 
+/****************************  search  *****************************/
+const searchInput = ref('')
+const tagbox = ref([])
+function onPressSpace() {
+  /* Space를 눌렀을 때
+   * 1. 단어의 첫 글자가 #으로 시작하면, 태그 만들기
+   *  - 태그박스로 만들고, tagbox에 추가 후 tagarea에 보여주기
+   *  - #으로 시작하는 단어 삭제시키기
+   */
+}
+
+function onPressEnter(event) {
+  event.preventDefault()
+}
+
+/******************************************************************/
+
 /****************************  list  *****************************/
 
 const searchedList = ref([
@@ -55,9 +73,83 @@ const searchedList = ref([
       'https://mblogthumb-phinf.pstatic.net/MjAyMDExMjRfMTA3/MDAxNjA2MjA1MDM0Nzkx.DrktdswN2SFmJQEaXdbhl4BnmzlN2oEN03hEHq_UUcQg.MIrlXoSMZsH0pRyuI0x3zIggxc3vlRcb53EYugvLqYkg.JPEG.sosohan_n/IMG_5367.JPG?type=w800',
     likes: 12,
     eyes: 4
+  },
+  {
+    id: 2,
+    nickname: '하나둘셋넷',
+    createdTime: '2017.03.02',
+    open: true,
+    subject: '스터디원 모집합니다.',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eius suscipit nobis, tempora ut rem est, ducimus aliquid, provident recusandae quo vero sapiente quod quia perspiciatis facilis ea enim mollitia?',
+    tag: ['백엔드', '자바', '호잇'],
+    user_img:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMDExMjRfMTA3/MDAxNjA2MjA1MDM0Nzkx.DrktdswN2SFmJQEaXdbhl4BnmzlN2oEN03hEHq_UUcQg.MIrlXoSMZsH0pRyuI0x3zIggxc3vlRcb53EYugvLqYkg.JPEG.sosohan_n/IMG_5367.JPG?type=w800',
+    likes: 12,
+    eyes: 4
+  },
+  {
+    id: 3,
+    nickname: '하나둘셋넷',
+    createdTime: '2017.03.02',
+    open: true,
+    subject: '스터디원 모집합니다.',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eius suscipit nobis, tempora ut rem est, ducimus aliquid, provident recusandae quo vero sapiente quod quia perspiciatis facilis ea enim mollitia?',
+    tag: ['백엔드', '자바', '호잇'],
+    user_img:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMDExMjRfMTA3/MDAxNjA2MjA1MDM0Nzkx.DrktdswN2SFmJQEaXdbhl4BnmzlN2oEN03hEHq_UUcQg.MIrlXoSMZsH0pRyuI0x3zIggxc3vlRcb53EYugvLqYkg.JPEG.sosohan_n/IMG_5367.JPG?type=w800',
+    likes: 12,
+    eyes: 4
+  },
+  {
+    id: 4,
+    nickname: '하나둘셋넷',
+    createdTime: '2017.03.02',
+    open: true,
+    subject: '스터디원 모집합니다.',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eius suscipit nobis, tempora ut rem est, ducimus aliquid, provident recusandae quo vero sapiente quod quia perspiciatis facilis ea enim mollitia?',
+    tag: ['백엔드', '자바', '호잇'],
+    user_img:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMDExMjRfMTA3/MDAxNjA2MjA1MDM0Nzkx.DrktdswN2SFmJQEaXdbhl4BnmzlN2oEN03hEHq_UUcQg.MIrlXoSMZsH0pRyuI0x3zIggxc3vlRcb53EYugvLqYkg.JPEG.sosohan_n/IMG_5367.JPG?type=w800',
+    likes: 12,
+    eyes: 4
+  },
+  {
+    id: 5,
+    nickname: '하나둘셋넷',
+    createdTime: '2017.03.02',
+    open: true,
+    subject: '스터디원 모집합니다.',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eius suscipit nobis, tempora ut rem est, ducimus aliquid, provident recusandae quo vero sapiente quod quia perspiciatis facilis ea enim mollitia?',
+    tag: ['백엔드', '자바', '호잇'],
+    user_img:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMDExMjRfMTA3/MDAxNjA2MjA1MDM0Nzkx.DrktdswN2SFmJQEaXdbhl4BnmzlN2oEN03hEHq_UUcQg.MIrlXoSMZsH0pRyuI0x3zIggxc3vlRcb53EYugvLqYkg.JPEG.sosohan_n/IMG_5367.JPG?type=w800',
+    likes: 12,
+    eyes: 4
+  },
+  {
+    id: 1,
+    nickname: '하나둘셋넷',
+    createdTime: '2017.03.02',
+    open: true,
+    subject: '스터디원 모집합니다.',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eius suscipit nobis, tempora ut rem est, ducimus aliquid, provident recusandae quo vero sapiente quod quia perspiciatis facilis ea enim mollitia?',
+    tag: ['백엔드', '자바', '호잇'],
+    user_img:
+      'https://mblogthumb-phinf.pstatic.net/MjAyMDExMjRfMTA3/MDAxNjA2MjA1MDM0Nzkx.DrktdswN2SFmJQEaXdbhl4BnmzlN2oEN03hEHq_UUcQg.MIrlXoSMZsH0pRyuI0x3zIggxc3vlRcb53EYugvLqYkg.JPEG.sosohan_n/IMG_5367.JPG?type=w800',
+    likes: 12,
+    eyes: 4
   }
 ])
 
+const pageInfo = ref({
+  currPage: 1,
+  totalPage: 5
+})
 /******************************************************************/
 </script>
 
@@ -67,7 +159,7 @@ const searchedList = ref([
       <h1 class="text-3xl font-extrabold">스터디</h1>
     </div>
     <div class="w-full rounded">
-      <!-- Tabs -->
+      <!-- 모집중 / 모집완료 tab -->
       <ul ref="tabsContainer" class="inline-flex w-full px-1 pt-2">
         <li
           class="px-4 py-2 -mb-px font-semibold text-gray-800 border-b-2 border-blue-400 rounded-t opacity-100"
@@ -81,13 +173,18 @@ const searchedList = ref([
       </ul>
 
       <div id="tab-contents">
+        <!-- 모집중 tab -->
         <div ref="openTabContent" class="p-4">
+          <!-- 검색 -->
           <div class="w-full flex justify-center p-1 mb-4">
             <div class="relative w-full">
               <input
                 type="text"
                 class="w-full backdrop-blur-sm bg-white/20 py-2 pl-10 pr-4 rounded-lg focus:outline-none border-2 border-gray-100 focus:border-violet-300 transition-colors duration-300"
                 placeholder="검색어를 입력하세요."
+                :value="searchInput"
+                @keyup.space="onPressSpace"
+                @keyup.enter="onPressEnter(event)"
               />
               <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                 <svg
@@ -108,6 +205,7 @@ const searchedList = ref([
               </div>
             </div>
           </div>
+          <!-- 정렬 tab -->
           <div class="w-full">
             <ul class="flex items-center gap-2 text-sm font-medium">
               <li class="flex-0.2">
@@ -136,6 +234,7 @@ const searchedList = ref([
               </li>
             </ul>
           </div>
+          <!-- 게시글 -->
           <div class="flex w-full min-h-screen items-start justify-center bg-white">
             <div class="p-6 w-full overflow-y-scroll overflow-x-hidden px-0">
               <table class="border-collapse border-y w-full table-auto text-left">
@@ -147,6 +246,7 @@ const searchedList = ref([
                   />
                 </tbody>
               </table>
+              <PaginationComponent :pageInfo="pageInfo" />
             </div>
           </div>
         </div>
---
+++ vue/src/components/board/item/PaginationComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({ pageInfo: Object })
+</script>
+
+<template>
+  <div class="flex items-center justify-center my-4">
+    <div class="flex justify-center items-center space-x-4">
+      <div
+        class="border rounded-md bg-gray-100 px-2 py-1 text-2xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
+      >
+        &lt;
+      </div>
+      <div class="text-slate-500">{{ pageInfo.currPage }} / {{ pageInfo.totalPage }}</div>
+      <div
+        class="border rounded-md bg-gray-100 px-2 py-1 text-2xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
+      >
+        &gt;
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
